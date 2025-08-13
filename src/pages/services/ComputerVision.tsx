3bb4a671e9a69915ed8b06ddadd643a4af42c943
mohbzour99-max <mohbzour99@gmail.com>
2025-08-13 17:52:56 +0300

    Create missing ComputerVision page component

---
+++ src/pages/services/ComputerVision.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ComputerVision = () => {
+  return (
+    <div>
+      <h1>Computer Vision Page</h1>
+      <p>Content for Computer Vision.</p>
+    </div>
+  );
+};
+
+export default ComputerVision;
