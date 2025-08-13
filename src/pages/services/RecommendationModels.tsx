237866b9c36aa1823487c006e08267c45b5548a4
mohbzour99-max <mohbzour99@gmail.com>
2025-08-13 17:54:06 +0300

    Create missing RecommendationModels page component

---
+++ src/pages/services/RecommendationModels.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const RecommendationModels = () => {
+  return (
+    <div>
+      <h1>Recommendation Models Page</h1>
+      <p>Content for Recommendation Models.</p>
+    </div>
+  );
+};
+
+export default RecommendationModels;
