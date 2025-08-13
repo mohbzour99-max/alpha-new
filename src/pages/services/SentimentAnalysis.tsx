9309691145cb60f2d65b9947a007b5a27a78b87b
mohbzour99-max <mohbzour99@gmail.com>
2025-08-13 17:53:17 +0300

    Create missing SentimentAnalysis page component

---
+++ src/pages/services/SentimentAnalysis.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SentimentAnalysis = () => {
+  return (
+    <div>
+      <h1>Sentiment Analysis Page</h1>
+      <p>Content for Sentiment Analysis.</p>
+    </div>
+  );
+};
+
+export default SentimentAnalysis;
