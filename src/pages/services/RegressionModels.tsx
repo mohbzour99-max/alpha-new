07cac83c290c0ef7900e103ca641721b11b90c8e
mohbzour99-max <mohbzour99@gmail.com>
2025-08-13 17:53:46 +0300

    Create missing RegressionModels page component

---
+++ src/pages/services/RegressionModels.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const RegressionModels = () => {
+  return (
+    <div>
+      <h1>Regression Models Page</h1>
+      <p>Content for Regression Models.</p>
+    </div>
+  );
+};
+
+export default RegressionModels;
