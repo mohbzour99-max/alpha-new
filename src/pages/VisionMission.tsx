9c1753c131c3b727792435c10f89ff84b7dc2064
mohbzour99-max <mohbzour99@gmail.com>
2025-08-17 13:16:32 +0300

    Create missing VisionMission page

---
+++ src/pages/VisionMission.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const VisionMission = () => {
+  return (
+    <div>
+      <h1>Vision and Mission Page</h1>
+      <p>Content for Vision and Mission.</p>
+    </div>
+  );
+};
+
+export default VisionMission;
