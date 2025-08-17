bd3ca772049150ea7bbf8a98f247ac9884508d1d
mohbzour99-max <mohbzour99@gmail.com>
2025-08-17 16:15:13 +0300

    Create missing Industries page component

---
+++ src/pages/Industries.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Industries = () => {
+  return (
+    <div>
+      <h1>Industries Page</h1>
+      <p>Content for Industries.</p>
+    </div>
+  );
+};
+
+export default Industries;
