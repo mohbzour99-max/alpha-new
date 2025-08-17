358504d8a70904cefaaa6b7e8a72c4e8753348b4
mohbzour99-max <mohbzour99@gmail.com>
2025-08-17 13:16:13 +0300

    Create missing About page

---
+++ src/pages/About.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>Content for About.</p>
+    </div>
+  );
+};
+
+export default About;
